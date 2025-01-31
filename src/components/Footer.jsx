fd6548d9d8166c4d68cf338c5cd5ff79a9e7b8cb
qwesakaido <qwesakaido70@gmail.com>
2025-01-31 16:08:15 +0300

    feat(project): added responsive design for mobile devices

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export function Footer() {
   return (
     <footer>
-      <div className="mx-auto flex max-h-[900px] max-w-[1440px] justify-start gap-10 px-[40px] py-[34px] font-['Futura'] font-bold md:max-xl:justify-between md:max-xl:gap-5 md:max-xl:w-1/2 md:max-xl:h-1/2">
+      <div className="mx-auto flex max-h-[900px] max-w-[1440px] justify-start gap-10 px-[40px] py-[34px] font-['Futura'] font-bold md:max-xl:justify-between md:max-xl:gap-5 md:max-xl:w-1/2 md:max-xl:h-1/2 sm:max-md:gap-5 sm:max-md:w-1/2 sm:max-md:h-1/2">
         <a
           className="text-[16px] text-[#444444] transition delay-150 hover:text-green-700"
           href="https://facebook.com"
